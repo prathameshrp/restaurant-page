51270313bb4064e45ddc5c7ece6b901ac2e6d171
prathamesh <prathamesh.math@gmail.com>
2024-08-17 01:02:58 +0530

    Importing page content from an external JS

---
+++ src/DOMContent.js
@@ -0,0 +1,40 @@
+{/* <div>
+<span>AUTHENTIC TRADITIONAL AND MODERN MEXICAN CUISINE IN TORONTO</span>
+<span class="aside-image"></span>
+</div>
+<div>
+    
+<p>Exclusive cuisines of Chihara Restaurant now in Birmingham Michigan. Experience the flavours of Chihara.</p>
+<img src="./assets/images/aside-image.jpg" alt="side image" class="second-div-image" height="250px">
+
+<p>Also available now - French, Japanese and other Asian cuisines. bringing a broad range of traditions and local cuisines.</p>
+</div> */}
+import asideImage from "./assets/images/aside-image.jpg";
+export function content(doc){
+    const parentDivs = [...Array(2)].map((x) => x = doc.createElement('div'));
+    const firstParentSpan1 = doc.createElement('span');
+    const firstParentSpan2 = doc.createElement('span');
+    firstParentSpan1.textContent = "AUTHENTIC TRADITIONAL AND MODERN MEXICAN CUISINE IN TORONTO";
+    firstParentSpan2.setAttribute("class", "aside-image");
+    parentDivs[0].appendChild(firstParentSpan1);
+    parentDivs[0].appendChild(firstParentSpan2);
+
+    // second div:
+    const secondParentP1 = doc.createElement('p');
+    const secondParentImg = doc.createElement('img');
+    const secondParentP2 = doc.createElement('p');
+
+    secondParentP1.textContent = "Exclusive cuisines of Chihara Restaurant now in Birmingham Michigan. Experience the flavours of Chihara.";
+    secondParentP2.textContent = "Also available now - French, Japanese and other Asian cuisines. bringing a broad range of traditions and local cuisines.";
+    secondParentImg.src = asideImage;
+    secondParentImg.setAttribute("class", "second-div-image");
+
+    parentDivs[1].appendChild(secondParentP1);
+    parentDivs[1].appendChild(secondParentImg);
+    parentDivs[1].appendChild(secondParentP2);
+
+    //append to #content
+    parentDivs.forEach((div) => doc.querySelector("#content").appendChild(div));
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { content } from "./DOMContent.js";
 import "./assets/fonts/stylesheet.css"
 import "./style.css";
+
+content(document);
 console.log("It's working");
